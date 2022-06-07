34c227845daa77aac1e57a5c8be0fb9b6747cdbc
Akshay Sharma <akshaysharma@akshay.sharma>
2022-06-07 17:38:10 +0530

    update organizations schema

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from statistics import mode
 from django.db import models
 
@@ -5,9 +6,9 @@ from django.db import models
 class Organization(models.Model):
 
     name = models.CharField(max_length=254, null=False, blank=False)
-    email = models.EmailField(max_length=30, null=True, blank=True, unique=True)
-    address = models.TextField(max_length=30, null=False, blank=False)
-    city = models.CharField(max_length=30, null=False, blank=False)
+    email = models.EmailField(max_length=30, null=False, blank=False, unique=True)
+    address = models.TextField(max_length=30, null=True, blank=True)
+    city = models.CharField(max_length=30, null=True, blank=True)
     country = models.CharField(max_length=30, null=False, blank=False)
     created_on = models.DateField(auto_now_add=True)
     updated_on = models.DateField(auto_now=True)
