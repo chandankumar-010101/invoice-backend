70499e8f096188332c0ce5362aadba6e32048eb9
Akshay Sharma <akshaysharma@akshay.sharma>
2022-06-15 12:36:40 +0530

    prod settings added

--- jasiri/settings/production.py
+++ jasiri/settings/production.py
@@ -1,2 +1,17 @@
 from .base import *
 DEBUG = False
+from decouple import config
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('TEST_DB_NAME'), 
+        'USER': config('TEST_USER'), 
+        'PASSWORD': config('TEST_PASSWORD'),
+        'HOST': config('TEST_HOST'), 
+        'PORT': config('TEST_PORT'),
+    }
+}
