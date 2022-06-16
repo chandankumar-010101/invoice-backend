124c1d1cfe9cfaf081aec58ff4a5e9a4e421d966
akshaysharma1801 <akshaysharma@akshay.sharma>
2022-06-16 17:51:33 +0530

    alternate contact code ,remaining

--- apps/customer/models.py
+++ apps/customer/models.py
@@ -2,7 +2,6 @@ from statistics import mode
 from django.db import models
 from .constants import CUSTOMER_TYPE_CHOICE
 from .constants import PAYMENT_TERM_CHOICE
-from django.core import validators
 
 
 class Customer(models.Model):
@@ -21,7 +20,7 @@ class Customer(models.Model):
     created_on = models.DateField(auto_now_add=True)
     updated_on = models.DateField(auto_now=True)
     primary_name = models.CharField(max_length=30, null=True, blank=True)
-    primary_role = models.CharField(max_length=30, null=True, blank=True) #change later
+    primary_role = models.PositiveSmallIntegerField(choices = CUSTOMER_TYPE_CHOICE, default=4)
     email = models.EmailField(max_length=254, unique=True)
     phone = models.CharField(max_length=30,null=True, blank=True, unique=True)
     payments_term = models.PositiveSmallIntegerField(choices = PAYMENT_TERM_CHOICE,
@@ -36,3 +35,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.full_name
+
+class AlternateContact(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, 
+                                        related_name='customer', default=None)
+    alternate_name = models.CharField(max_length=30, null=True, blank=True)
+    alternate_role = models.PositiveSmallIntegerField(choices = CUSTOMER_TYPE_CHOICE, 
+                                                default=4)
+    alternate_email = models.EmailField(max_length=254, unique=True)
+    alternate_phone = models.CharField(max_length=30,null=True, blank=True)
--- apps/customer/serializers.py
+++ apps/customer/serializers.py
@@ -1,20 +1,40 @@
-from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 from apps.customer.models import Customer 
+from apps.customer.models import AlternateContact
+
+
+class ALternateContactSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AlternateContact
+        fields = '__all__'
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+
+    alternate_contact = ALternateContactSerializer()
 
     class Meta:
         model = Customer
         fields = '__all__'
 
     def validate_phone(self,phone):
-        print(phone)
         
         if len(phone) > 10 or len(phone) < 10:
             raise serializers .ValidationError('number cannot be less or greater than 10')
-            return validate_data     
+        return phone     
+
+    def create(self, validated_data):
+        # print(validated_data)
+        alternate_record = validated_data.get('alternate_contact')
+        print('==================')
+        print(alternate_record)
+        instance = Customer.objects.create(**validated_data)
+        AlternateContact.objects.create(**alternate_record)
+        # alternate_obj.customer = instance
+        # alternate_obj.save()
+        instance.save()
+        return instance
 
 class CustomerFilterSerializer(serializers.ModelSerializer):
 
