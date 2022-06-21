9d897da3b98bf8c3dd0bb3d56f8f8eb1830112cd
Aftab Hussain <aftab4evr786@gmail.com>
2022-06-21 16:07:58 +0530

    swagger setup

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,69 +1,71 @@
 
+
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from drf_yasg.utils import swagger_auto_schema
 
-from rest_framework import generics
-from .models import Organization
-from .serializer import OrganizationSerializer, PasswordchangeSerializer
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
+from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login, logout
+
+from .utils import get_jwt_tokens_for_user
 import apps.account.user_service as user_service
 import apps.account.response_messages as resp_msg
-from .utils import get_jwt_tokens_for_user
-from .models import UserProfile
-from .models import Organization
-from .serializer import OrganizationSerializer
-from .serializer import SignupSerializer
-from .serializer import LoginSerializers
-from .serializer import UserProfileSerializer
-from .serializer import UserCreateSerializer
-from .serializer import UserProfileListSerializer
-from rest_framework.permissions import IsAuthenticated
-from .permissions import IsAdminOnly
-from .serializer import ProfileupdateSerializer
-from .serializer import PasswordchangeSerializer
-from django.contrib.auth import get_user_model
+from .models import UserProfile,Organization
+from .serializer import (
+    OrganizationSerializer,
+    SignupSerializer, LoginSerializers,
+    UserProfileSerializer, UserCreateSerializer,
+    UserProfileListSerializer, ProfileupdateSerializer,
+    PasswordchangeSerializer
+)
 
+from .permissions import IsAdminOnly
 from .schema import (
     login_schema,
     change_password_schema,
 )
 
 # Create your views here.
+
+
 class OrganizationListView(generics.ListAPIView):
 
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
 
+
 class SignupView(APIView):
     """ Create new User and organization.
-    
+
     Register a user and organization and create
     a user profile.
     """
-    
+
     def post(self, request, *args, **kwargs):
         response = {}
-        serializer = SignupSerializer(data = request.data)
+        serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
             # create Organization
             org = user_service.create_organization(request)
             user_type = 2
 
-            #create user
+            # create user
             try:
-                user = user_service.create_admin_user(request,user_type)
+                user = user_service.create_admin_user(request, user_type)
             except Exception as e:
                 print(str(e))
-                return Response({'error':resp_msg.USER_CREATION_UNSUCCESSFULL}, 
-                status=status.HTTP_400_BAD_REQUEST)
+                return Response({'error': resp_msg.USER_CREATION_UNSUCCESSFULL},
+                                status=status.HTTP_400_BAD_REQUEST)
 
-            #create profile
+            # create profile
             profile = user_service.create_user_profile(request, user, org)
-            if (org is not None and 
-                user is not None and 
-                profile is not None):
+            if (org is not None and
+                user is not None and
+                    profile is not None):
                 queryset = UserProfile.objects.get(email=profile.email)
                 serializer = UserProfileSerializer(queryset)
                 response['profile'] = serializer.data
@@ -73,10 +75,10 @@ class SignupView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-       
+
 class LoginView(APIView):
     """ Login a user in the platform.
-    
+
     Login the user in the platform and generate access and 
     refresh jwt token for users.
     """
@@ -85,7 +87,7 @@ class LoginView(APIView):
         response = {}
         email = request.data.get('email')
         password = request.data.get('password')
-        serializer = LoginSerializers(data = request.data)
+        serializer = LoginSerializers(data=request.data)
         if serializer.is_valid():
             user = authenticate(username=email, password=password)
             if user is not None:
@@ -98,16 +100,18 @@ class LoginView(APIView):
                 response['user_type'] = user.user_type
                 response['access'] = token.get('access')
                 response['refresh'] = token.get('refresh')
-                response['last_login'] = user.last_login.strftime("%H:%M %P, %d %b %Y")
+                response['last_login'] = user.last_login.strftime(
+                    "%H:%M %P, %d %b %Y")
                 return Response(response, status=status.HTTP_200_OK)
             else:
-                return Response({'detail':[resp_msg.INVALID_EMAIL_PASSWORD]}, 
-                            status=status.HTTP_400_BAD_REQUEST)
+                return Response({'detail': [resp_msg.INVALID_EMAIL_PASSWORD]},
+                                status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserCreateView(APIView):
     """ Create new User for an organization.
-    
+
     Register a user and create a user profile.
     validation for only admin user can create new user.
     """
@@ -115,28 +119,30 @@ class UserCreateView(APIView):
     permission_classes = (IsAuthenticated & IsAdminOnly,)
 
     def post(self, request, *args, **kwargs):
-        serializer = UserCreateSerializer(data = request.data)
+        serializer = UserCreateSerializer(data=request.data)
         if serializer.is_valid():
             user_org = UserProfile.objects.get(user=request.user).organization
 
             try:
-                user =  user_service.create_user_with_role(request)
+                user = user_service.create_user_with_role(request)
             except Exception as e:
                 print(str(e))
-                return Response({'error':resp_msg.USER_CREATION_UNSUCCESSFULL}, 
-                status=status.HTTP_400_BAD_REQUEST)
+                return Response({'error': resp_msg.USER_CREATION_UNSUCCESSFULL},
+                                status=status.HTTP_400_BAD_REQUEST)
 
             if user:
-                profile = user_service.create_user_profile(request, user, user_org)
+                profile = user_service.create_user_profile(
+                    request, user, user_org)
                 queryset = UserProfile.objects.get(pk=profile.pk)
                 serializer = UserProfileSerializer(queryset)
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
 class UserListView(generics.ListAPIView):
     """ List of user from an organization.
-    
+
     List of user from an organization, only admin user
     are allowed.
     """
@@ -151,6 +157,7 @@ class UserListView(generics.ListAPIView):
         queryset = UserProfile.objects.filter(organization=organization)
         return queryset
 
+
 class LogoutView(APIView):
 
     permission_classes = (IsAuthenticated, )
@@ -159,9 +166,10 @@ class LogoutView(APIView):
         logout(request)
         return Response({'success': True}, status=status.HTTP_200_OK)
 
+
 class ProfileupdateView(APIView):
-     
-     def post(self, request, *args, **kwargs):
+
+    def post(self, request, *args, **kwargs):
         user_name = request.data.get('user_name')
         name = request.data.get('name')
         email = request.data.get('email')
@@ -169,30 +177,31 @@ class ProfileupdateView(APIView):
         role = request.data.get('role')
         profile_status = request.data.get('status')
 
-        serializer = ProfileupdateSerializer(data = request.data)
+        serializer = ProfileupdateSerializer(data=request.data)
         if serializer.is_valid():
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-                        
+
+
 class ChangePasswordView(APIView):
-    
+
     permission_classes = (IsAuthenticated, )
 
     def get_object(self, queryset=None):
         return self.request.user
 
     @swagger_auto_schema(request_body=change_password_schema, operation_description='Change Password')
-    def post(self,request,*args,**kwargs):
+    def post(self, request, *args, **kwargs):
         User = get_user_model()
-        serializer = PasswordchangeSerializer(data = request.data)
+        serializer = PasswordchangeSerializer(data=request.data)
         if serializer.is_valid():
             current_password = request.data.get('current_password')
             new_password = request.data.get('new_password')
             user = User.objects.get(email=request.user.email)
             if not user.check_password(current_password):
                 return Response({"detail": resp_msg.INCORRECT_PASSWORD},
-                status=status.HTTP_400_BAD_REQUEST)
+                                status=status.HTTP_400_BAD_REQUEST)
             else:
                 print(self)
                 self.get_object().set_password(new_password)
