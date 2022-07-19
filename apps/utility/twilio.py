a8f170422d8c5110a851df6c2689f1e26da31a4f
Aftab Hussain <aftab.hussain@oodles.io>
2022-07-19 11:15:33 +0530

    Worked on csv part

--- apps/utility/twilio.py
+++ apps/utility/twilio.py
@@ -13,13 +13,13 @@ account_sid = config('TWILIO_ACCOUNT_SID')
 auth_token = config('TWILIO_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
-def send_media_on_whatsapp(phone_no):
-    message = client.messages.create(
-        media_url=['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
-        from_='whatsapp:+14155238886',
-        to='whatsapp:{}'.format(phone_no)
-    )
-    print(message.sid)
+# def send_media_on_whatsapp(phone_no):
+#     message = client.messages.create(
+#         media_url=['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
+#         from_='whatsapp:+14155238886',
+#         to='whatsapp:{}'.format(phone_no)
+#     )
+#     print(message.sid)
 
 
 def send_message_on_whatsapp(invoice):
--- jasiri/settings/base.py
+++ jasiri/settings/base.py
@@ -182,11 +182,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_EMAIL_ID= config('EMAIL_HOST_USER')
+EMAIL_EMAIL_ID= "no-reply@jasiricap.com"
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD =  config('EMAIL_HOST_PASSWORD')
 
