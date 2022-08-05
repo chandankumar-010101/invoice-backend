0c8afa06e8470128ac89a367fb78fdbda456efdc
Aftab Hussain <aftab.hussain@oodles.io>
2022-08-05 14:17:31 +0530

    Worked on push notifications

--- apps/account/views.py
+++ apps/account/views.py
@@ -348,6 +348,6 @@ class GetDetailsView(APIView):
         payment_method['is_card_payment']= request.user.payment_method.is_card_payment if hasattr( request.user,'payment_method') else False
         payment_method['is_mobile_money']= request.user.payment_method.is_mobile_money if hasattr(request.user,'payment_method') else False
         payment_method['auto_payment_reminder']= request.user.payment_method.auto_payment_reminder if hasattr(request.user,'payment_method') else False
-        response['roles'] = user.roles_permission_user.roles
+        response['roles'] = request.user.roles_permission_user.roles
         response['payment_method'] = payment_method
         return Response(response, status=status.HTTP_200_OK)
