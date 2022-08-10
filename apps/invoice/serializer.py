b3d56c53292a7b043782afb937602c5f2a1ae732
Aftab Hussain <aftab.hussain@oodles.io>
2022-08-10 12:40:38 +0530

    Worked on csv part

--- apps/invoice/serializer.py
+++ apps/invoice/serializer.py
@@ -136,7 +136,6 @@ class CardSerializer(serializers.Serializer):
     def create(self,validated_data):
         request = self.context.get('request')
         params = request.data
-
         return CardDetail.objects.create(
             user=request.user,
             payment_type = params['payment_type'],
